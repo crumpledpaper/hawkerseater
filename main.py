32bb4c976bacf2a855caaca34b68535af24fe99f
crumpledpaper <ong.junhao.bryan@dhs.sg>
2014-04-18 14:16:47 +0800

    added channels api

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 import jinja2
 import webapp2
 
+from google.appengine.api import channel
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates')),
     extensions=['jinja2.ext.autoescape'],
